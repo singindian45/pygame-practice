237704528a2717539357a8706734a697a4e207bf
Volty <pranshuj73@gmail.com>
2020-09-05 21:05:22 +0530

    moved all functions to game_function.py
    Also, prettified all dialogues

--- pokemon.py
+++ pokemon.py
@@ -6,93 +6,7 @@ import json
 import numpy as np
 import sys
 import time
-
-
-def printf(sentence: str):
-    # Print one character at a time
-    for char in sentence:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        # time.sleep(0.08)
-    print()
-
-
-def inputf(sentence: str):
-    # Print one character at a time and take in the output
-    for char in sentence:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        # time.sleep(0.08)
-
-    choice = input()
-    return choice
-
-
-def dialogue(dialogues: dict):
-    for speaker in dialogues:
-        space = 13
-        if len(speaker) > space:
-            space = len(speaker)
-        dialogue = dialogues[speaker]
-
-        if isinstance(dialogue, str):
-            printf(f"{speaker.ljust(space)}: {dialogue}")
-            print()
-        elif isinstance(dialogue, list):
-            printf(f"{speaker.ljust(space)}: {dialogue[0]}")
-            for line in dialogue[1:]:
-                printf(" "*(space+2) + f"{line}")
-            print()
-
-
-def y_or_n(choice: str):
-    if choice.lower().strip() == "y" or choice.lower().strip() == "yes" or choice == "":
-        return True
-    else:
-        return False
-
-
-def confirm(question: str):
-    question_response = inputf(question)
-    choice = inputf(f"Proceed with '{question_response}'?[Y/n]\t")
-    if choice.lower().strip() != "y" and choice.lower().strip() != "yes" and choice.lower().strip() != "":
-        confirm_decision = inputf(f"Do you want to change your choice?[Y/n]\t")
-        if confirm_decision.lower().strip() == "y" or confirm_decision.lower().strip() == "yes" or confirm_decision.lower().strip() == "":
-            return confirm(question)
-    else:
-        return question_response
-
-
-def event_choice(events: dict):
-    count = len(events)
-    choices = [i for i in range(count)]
-    for n, event in zip(choices, events.keys()):
-        printf(f"[{n}] {event}")
-
-    options = "|".join(str(n) for n in choices)  # just a string showing possible options
-    choice = inputf(f"Enter your choice:[{options}]\t").lower().strip()
-
-    def check_instance(event):
-        if isinstance(event, str):
-            printf(f"{event}")
-        elif isinstance(event, dict):
-            event_choice(event)
-        elif isinstance(event, list):
-            for inner_event in event:
-                check_instance(inner_event)
-
-    if choice.isnumeric():
-        if int(choice) in choices:
-            decision = list(events.keys())[int(choice)]
-            check_instance(events[decision])
-
-        else:
-            printf("Please choose a valid option.")
-            event_choice(events)
-
-    else:
-        printf("That is not a valid choice! Try again with a number.")
-        event_choice(events)
+from game_functions import *
 
 
 def exit_handler():
@@ -103,17 +17,11 @@ def exit_handler():
 atexit.register(exit_handler)
 
 
-def game_save(Trainer):
-    # to be completed
-    with open("savefile.txt", "w") as save:
-        save.write()
-
-
 # -------------------------------------------------------
 
 
 class Trainer:
-    def __init__(self, name: str, pokemon: dict, location: str, money = 1000):
+    def __init__(self, name: str, pokemon: dict, location: str, money=1000):
         self.name = name
         self.money = money
         self.pokemon = pokemon
@@ -127,9 +35,9 @@ class Trainer:
 
     # def PokemonInventory(self): just commenting for a test
 
-    #def CapturePokemon(self):
-        #global capture
-        #capture = True
+    # def CapturePokemon(self):
+    # global capture
+    # capture = True
 
 
 class Pokemon:
@@ -298,8 +206,8 @@ dialogue(
             "This world is inhabited by creatures called Pokemon!",
             "For some people, Pokemon are pets. Others use them for fights. Myself...",
             "I study Pokemon as a profession.",
-            "First, what is your name?"
-          ]
+            "First, what is your name?",
+        ],
     }
 )
 
@@ -307,20 +215,27 @@ player = Trainer(confirm("Enter your name:\t"), 0, {}, "Pallet Town")
 printf(f"Right! So your name is {player.name}!")
 print()
 
-dialogue({
-    "Prof.Oak": ["This is my grandson. He's been your rival since you were a baby.", "...Erm, what is his name again?"]
-}
+dialogue(
+    {
+        "Prof.Oak": [
+            "This is my grandson. He's been your rival since you were a baby.",
+            "...Erm, what is his name again?",
+        ]
+    }
 )
 
 rival = Trainer(confirm("Enter your rival's name:\t"), 0, {}, player.location)
 printf(f"That's right! I remember now! His name is {rival.name}!")
 print()
 
-dialogue({
-    "Prof.Oak": "Your very own POKEMON legend is about to unfold! A world of dreams and adventures with POKEMON awaits! Let's go!"
-})
+dialogue(
+    {
+        "Prof.Oak": "Your very own POKEMON legend is about to unfold! A world of dreams and adventures with POKEMON awaits! Let's go!"
+    }
+)
 
-print("""Scene: You are in your bedroom now. You go downstairs to find your mother watching a program on T.V.
+print(
+    """Scene: You are in your bedroom now. You go downstairs to find your mother watching a program on T.V.
 Talk to her?
 """
 )
@@ -328,7 +243,7 @@ Talk to her?
 event_choice(
     {
         "Yes": "Mother: Right. All boys leave home some day. It said so on TV.\nProf.Oak, next door, is looking for you.",
-        "No": "You prepare to leave the house but your mother stops you and tells you that Prof.Oak, next door, is looking for you.\n"
+        "No": "You rude! Go talk to her anyway.\nMother: Right. All boys leave home some day. It said so on TV.\nProf.Oak, next door, is looking for you.",
     }
 )
 
@@ -354,64 +269,82 @@ event_choice(
             "You see a young kid wearing shorts, Talk to him?",
             {
                 "Yes": "You talk to the kid\nYoungster Jimmy: When you hear grass rustling, it's usually a pokemon.\nProf.Oak told us so in his orientation class.",
-                "No": "You aimlessly roam around the town waiting for Prof.Oak to show up"
-            }
+                "No": "You aimlessly roam around the town waiting for Prof.Oak to show up",
+            },
         ],
         "Go out and explore the town": [
             "You start exploring the town and try to ask if anyone has seen where Prof. Oak has gone.",
             "You see a young kid wearing shorts, Talk to him?",
             {
                 "Yes": "You talk to the kid\nYoungster Jimmy: When you hear grass rustling, it's usually a pokemon.\nProf.Oak told us so in his orientation class.",
-                "No": "You aimlessly roam around the town waiting for Prof.Oak to show up"
-            }
+                "No": "You aimlessly roam around the town waiting for Prof.Oak to show up",
+            },
         ],
         f"Go to {player.name}'s house": [
             f"You enter {player.name}'s house.",
             {
-                "Talk to mom?": "Mom: Prof. Oak is the authority on Pokemon, Many pokemon trainers hold him in high regard.",
+                "Talk to mom?": "Mom: Prof. Oak is the authority on Pokemon, Many pokemon trainers hold him in high regard.\nYou then come out of your house and explore the town again.",
                 "Go back to town": "You come out of your house and roam aimlessly hoping Prof. Oak would arrive soon.",
-            }
+            },
+        ],
+    }
+)
+
+
+printf(
+    "As you're waiting outside in the town, you hear rustling grass in distance. Do you check it out?\n"
+)
+event_choice(
+    {
+        "Yes": "You carefully tiptoe towards the grass so you don't scare the pokemon in grass",
+        "No": "You decide to not pay attention to the rustling, but after a few minutes your curiosity gets the better of you\nYou carefully tiptoe towards the grass.",
+    }
+)
+
+printf(
+    f"""You see the silhouette of a mouse like pokemon feasting on berries.
+'That's a pikachu!' you shout in excitment. The wild Pikachu notices you and prepares to attack.
+You notice the electricity crackling near Pikachu's cheeks and suddenly you hear someone shout
+"""
+)
+
+dialogue({"Prof.Oak": "Hey! Wait! Don't get closer!"})
+printf(
+    "Prof.Oak throws a pokeball at Pikachu... The pokeball shakes a few times before sparking slightly indicating Pikachu has been captured."
+)
+
+dialogue(
+    {
+        "Prof.Oak": [
+            "Whew...",
+            "A pokemon can appear anytime in tall grass. You need your own pokemon for your protection.",
+            "I know!",
         ]
     }
 )
 
-
-printf("As you're waiting outside in the town, you hear rustling grass in distance. Do you check it out?")
-event_choice(
-    {
-        "Yes": "You carefully tiptoe towards the grass so you don't scare the pokemon in grass",
-        "No" : "You decide to not pay attention to the rustling, but after a few minutes your curiosity gets the better of you\nYou carefully tiptoe towards the grass."
-    }
-)
-
-printf(f"""You see the silhouette of a mouse like pokemon feasting on berries.
-'That's a pikachu!' you shout in excitment. The wild Pikachu notices you and prepares to attack.
-You notice the electricity crackling near Pikachu's cheeks and suddenly you hear someone shout
-
-Prof.Oak: Hey! Wait! Don't get closer!
-Prof.Oak throws a pokeball at Pikachu... The pokeball shakes a few times before sparking slightly indicating Pikachu has been captured.
-
-Prof.Oak: Whew...
-Prof.Oak: A pokemon can appear anytime in tall grass. You need your own pokemon for your protection.
-I know!
-
+printf(
+    """
 Prof.Oak asks you to come with him to his lab, to which you happilly agree.
 
 ==========================================================================================================
 
 At Prof.Oak's Lab:
+"""
+)
 
-{rival.name}: Gramps! I'm fed up with waiting!
-
-Prof.Oak: {rival.name}? Let me think... Oh, that's right, I told you to come!
-          Just wait!
-          
-          Here, {player.name}! There are 3 Pokemon here! Haha! They are inside the pokeballs.
-          When I was young, I was a serious Pokemon trainer.
-          In my old age I have only 3 left, but you can have one! Choose!
-
-
-{rival.name}: Hey! Gramps! What about me?
-
-Prof.Oak: Be patient! {rival.name}, you can have one too!
-""")
+dialogue(
+    {
+        rival.name: "Gramps! I'm fed up with waiting!",
+        "Prof.Oak": [
+            f"{rival.name}? Let me think... Oh, that's right, I told you to come!",
+            "Just wait!",
+            "",
+            f"Here, {player.name}! There are 3 Pokemon here! Haha! They are inside the pokeballs."
+            "When I was young, I was a serious Pokemon trainer."
+            "In my old age I have only 3 left, but you can have one! Choose!",
+        ],
+        rival.name: "Hey! Gramps! What about me?",
+        "Prof.Oak": f"Be patient! {rival.name}, you can have one too!",
+    }
+)
