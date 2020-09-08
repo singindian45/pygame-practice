ba4754c4fbe3f69cdc26ba2da6a1cfe678860609
Maxix25 <maximiliano.besoain@gmail.com>
2020-09-08 17:39:00 -0300

    Bug fixes and separate functions for each turn

--- classes.py
+++ classes.py
@@ -1,6 +1,10 @@
 import numpy as np
 import time
 from game_functions import printf, inputf, y_or_n
+from pokemons import *
+
+turn=0
+loop=False
 
 class Trainer:
     def __init__(self, name: str, location: str, money=1000, pokemon={}):
@@ -33,7 +37,7 @@ class Pokemon:
         self.bars = 50  # Amount of health bars
         self.exp = exp
 
-    def fight(self, Pokemon2):
+    def local_fight(self, Pokemon2):
         # This allow 2 pokemons fight each other
         # Print fight information
         self.Pokemon2 = Pokemon2
@@ -72,62 +76,85 @@ class Pokemon:
                     Pokemon2.attack /= 1.5
                     string_1_attack = "It's super effective!"
                     string_2_attack = "It's not very effective..."
-        while (self.bars > 0) and (Pokemon2.bars > 0):
-            # Print the health of each pokemon
-            print(f"\n{self.name}\t\tHLTH\t{self.health}")
-            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
+        def Pokemon1_turn():
+            while (self.bars > 0) and (Pokemon2.bars > 0):
+                # Print the health of each pokemon
+                if loop==False:
+                    print(f"\n{self.name}\t\tHLTH\t{self.health}")
+                    print(f"\n{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
 
-            print(f"Go {self.name}!")
-            for i, x in enumerate(self.moves):
-                print(f"{i+1}.", x)
-            index = int(input("Pick a move: "))
-            printf(f"{self.name} used {self.moves[index-1]}!")
-            time.sleep(1)
-            printf(string_1_attack)
+                print(f"Go {self.name}!")
+                for i, x in enumerate(self.moves):
+                    print(f"{i+1}.", x)
+                index = int(input("Pick a move: "))
+                printf(f"{self.name} used {self.moves[index-1]}!")
+                time.sleep(1)
+                printf(string_1_attack)
 
-            # Determine damage
-            Pokemon2.bars -= self.attack
-            Pokemon2.health = ""
+                # Determine damage
+                Pokemon2.bars -= self.attack
+                Pokemon2.health = ""
 
-            # Add back bars plus give a defense boost
-            for j in range(int(Pokemon2.bars + 0.1 * Pokemon2.defense)):
-                Pokemon2.health += "="
-            time.sleep(1)
-            print(f"{self.name}\t\tHLTH\t{self.health}")
-            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
-            time.sleep(0.5)
+                # Add back bars plus give a defense boost
+                for j in range(int(Pokemon2.bars + 0.1 * Pokemon2.defense)):
+                    Pokemon2.health += "="
+                time.sleep(1)
+                global turn
+                turn=1
+                time.sleep(0.5)
 
-            # Check if the pokemon has fainted
-            if Pokemon2.bars <= 0:
-                printf(f"\n... {Pokemon2.name} fainted")
-                break
-            # ----------------------------------If Pokemon2's hasn't fainted, then it's Pokemon2's turn-------------------------
-            print(f"Go {Pokemon2.name}!")
-            for i, x in enumerate(Pokemon2.moves):
-                print(f"{i+1}.", x)
-            index = int(input("Pick a move: "))
-            printf(f"{Pokemon2.name} used {Pokemon2.moves[index-1]}!")
-            time.sleep(1)
-            printf(string_2_attack)
+                # Check if the pokemon has fainted
+                if Pokemon2.bars <= 0:
+                    printf(f"\n... {Pokemon2.name} fainted")
+                    turn=10
+                    break
+                else:
+                    break
+        # ----------------------------------If Pokemon2's hasn't fainted, then it's Pokemon2's turn-------------------------
+        def Pokemon2_turn():
+            while (self.bars>0) and (Pokemon2.bars>0):
+                print(f"\nGo {Pokemon2.name}!")
+                for i, x in enumerate(Pokemon2.moves):
+                    print(f"{i+1}.", x)
+                index = int(input("Pick a move: "))
+                printf(f"{Pokemon2.name} used {Pokemon2.moves[index-1]}!")
+                time.sleep(1)
+                printf(string_2_attack)
 
-            # Determine damage
-            self.bars -= Pokemon2.attack
-            self.health = ""
+                # Determine damage
+                self.bars -= Pokemon2.attack
+                self.health = ""
 
-            # Add back bars plus give a defense boost
-            for j in range(int(self.bars + 0.1 * self.defense)):
-                self.health += "="
-            time.sleep(1)
-            print(f"{self.name}\t\tHLTH\t{self.health}")
-            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
-            time.sleep(0.5)
+                # Add back bars plus give a defense boost
+                for j in range(int(self.bars + 0.1 * self.defense)):
+                    self.health += "="
+                time.sleep(1)
+                print(f"{self.name}\t\tHLTH\t{self.health}")
+                print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
+                global loop
+                global turn
+                loop=True
+                turn=0
+                time.sleep(0.5)
 
-            # Check if the pokemon has fainted
-            if self.bars <= 0:
-                printf("\n..." + self.name + " fainted")
-                break
-        money = np.random.choice(5000)
-        printf(f"Opponent paid you {money}.")
+                # Check if the pokemon has fainted
+                if self.bars <= 0:
+                    printf("\n..." + self.name + " fainted")
+                    turn=10
+                    print(turn)
+                    money = np.random.choice(5000)
+                    printf(f"Opponent paid you {money}.")
+                    break
+                else:
+                    Pokemon1_turn()
+
+                
+        if turn==0:
+                Pokemon1_turn()
+        if turn==1:
+                Pokemon2_turn()
+
+
     def PokemonState(self):
         print(
             "Your pokemon has: ",
@@ -159,40 +186,41 @@ Venusaur = Pokemon('Venusaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Earthquake',
 # Venusaur.PokemonState()
 
 #
-def pkmn_battle(player, wild_pkmn):
-    for pokemon in player.pokemon:
-        pokemon_obj = player.pokemon[pokemon]
-        if len(pokemon_obj.health) != 0:
-            pokemon_obj.fight(wild_pkmn)
-            break
+# def pkmn_battle(player, wild_pkmn):
+#     for pokemon in player.pokemon:
+#         pokemon_obj = player.pokemon[pokemon]
+#         if len(pokemon_obj.health) != 0:
+#             pokemon_obj.fight(wild_pkmn)
+#             break
 
-    if y_or_n(inputf(f"Would you like to catch {wild_pkmn.name}?[Y/n]\t")):
-        if y_or_n(inputf(f"Would you like to nickname {wild_pkmn.name}?[Y/n]\t")):
-            nick = inputf(f"Enter {wild_pkmn.name}'s nickname:\t")
-            player.pokemon[nick] = wild_pkmn
-        else:
-            player.pokemon[wild_pkmn.name] = wild_pkmn
-
-
-def trainer_battle(player, trainer):
-    for pokemon in player.pokemon:
-        pokemon_obj = player.pokemon[pokemon]
-        print(player.pokemon)
-        while len(pokemon_obj.health) != 0:
-            for rival_pkmn in rival.pokemon:
-                rival_pkmn_obj = rival.pokemon[rival_pkmn]
-                pokemon_obj.fight(rival_pkmn_obj)
+#     if y_or_n(inputf(f"Would you like to catch {wild_pkmn.name}?[Y/n]\t")):
+#         if y_or_n(inputf(f"Would you like to nickname {wild_pkmn.name}?[Y/n]\t")):
+#             nick = inputf(f"Enter {wild_pkmn.name}'s nickname:\t")
+#             player.pokemon[nick] = wild_pkmn
+#         else:
+#             player.pokemon[wild_pkmn.name] = wild_pkmn
 
 
-def give_pkmn(player, pkmn):
-    printf(f"{player.name} received {pkmn.name}!")
-    if y_or_n(inputf(f"Would you like to nickname {pkmn.name}?[Y/n]\t")):
-        nick = inputf(f"Enter {pkmn.name}'s nickname:\t")
-        player.pokemon[nick] = pkmn
-    else:
-        player.pokemon[pkmn.name] = pkmn
+# def trainer_battle(player, trainer):
+#     for pokemon in player.pokemon:
+#         pokemon_obj = player.pokemon[pokemon]
+#         print(player.pokemon)
+#         while len(pokemon_obj.health) != 0:
+#             for rival_pkmn in rival.pokemon:
+#                 rival_pkmn_obj = rival.pokemon[rival_pkmn]
+#                 pokemon_obj.fight(rival_pkmn_obj)
 
-player = Trainer("Klaus", "Pallet Town", 0, {"venusaur": Charizard})
-rival = Trainer("Ben", "Pallet Town", 0, {"venusaur": Venusaur, "blastoise": Blastoise})
 
-trainer_battle(player, rival)
+# def give_pkmn(player, pkmn):
+#     printf(f"{player.name} received {pkmn.name}!")
+#     if y_or_n(inputf(f"Would you like to nickname {pkmn.name}?[Y/n]\t")):
+#         nick = inputf(f"Enter {pkmn.name}'s nickname:\t")
+#         player.pokemon[nick] = pkmn
+#     else:
+#         player.pokemon[pkmn.name] = pkmn
+
+# player = Trainer("Klaus", "Pallet Town", 0, {"venusaur": Charizard})
+# rival = Trainer("Ben", "Pallet Town", 0, {"venusaur": Venusaur, "blastoise": Blastoise})
+
+# trainer_battle(player, rival)
+Charizard.local_fight(Venusaur)
